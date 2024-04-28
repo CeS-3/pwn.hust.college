63a8bc36064d277f534c224e5d6b0b8541e1a0c0
CeS-3 <1540962271@qq.com>
2024-04-28 15:15:16 +0800

    Make syllabus visible even if not authed

--- dojo_plugin/pages/course.py
+++ dojo_plugin/pages/course.py
@@ -199,8 +199,7 @@ def grade(dojo, users_query):
 
 @course.route("/dojo/<dojo>/course")
 @dojo_route
-@authed_only
-def view_course(dojo):
+def view_course(dojo, resource=None):
     if not dojo.course:
         abort(404)
 
@@ -213,14 +212,17 @@ def view_course(dojo):
         user = get_current_user()
         name = "Your"
 
-    grades = next(grade(dojo, user))
+    grades = {}
     identity = {}
 
-    student = DojoStudents.query.filter_by(dojo=dojo, user=user).first()
-    identity["identity_name"] = dojo.course.get("student_id", "Identity")
-    identity["identity_value"] = student.token if student else None
+    if user:
+        grades = next(grade(dojo, user))
 
-    return render_template("course.html", name=name, **grades, **identity, dojo=dojo)
+        student = DojoStudents.query.filter_by(dojo=dojo, user=user).first()
+        identity["identity_name"] = dojo.course.get("student_id", "Identity")
+        identity["identity_value"] = student.token if student else None
+
+    return render_template("course.html", name=name, **grades, **identity, user=user, dojo=dojo)
 
 
 @course.route("/dojo/<dojo>/course/identity", methods=["PATCH"])
