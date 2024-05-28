286a6bf0c4acfb7a1f9bfca62a2c2111b94be9a9
CeS-3 <1540962271@qq.com>
2024-05-28 21:48:42 +0800

    Update workspace.py
    0001-Workspace-Fix-view-only-in-desktop.patch

--- dojo_plugin/pages/workspace.py
+++ dojo_plugin/pages/workspace.py
@@ -43,13 +43,16 @@ def view_desktop():
     view_password = container_password(container, "desktop", "view")
 
     if user_id and password:
-        if not hmac.compare_digest(password, interact_password) and not hmac.compare_digest(password, view_password):
+        if hmac.compare_digest(password, interact_password):
+            view_only = False
+        elif hmac.compare_digest(password, view_password):
+            view_only = True
+        else:
             abort(403)
         password = password[:8]
     else:
         password = interact_password[:8]
 
-    view_only = user_id is not None
     service = "~".join(("desktop", str(user.id), container_password(container, "desktop")))
 
     vnc_params = {
