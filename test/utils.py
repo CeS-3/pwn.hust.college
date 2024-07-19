318fa33c5aa332048f88d4e8264803d55331bb3f
CeS-3 <1540962271@qq.com>
2024-07-19 11:43:45 +0800

    update test CI port from 8080 to 80

--- test/utils.py
+++ test/utils.py
@@ -6,7 +6,7 @@ import re
 import os
 
 PROTO="http"
-HOST="localhost.pwn.hust.college:8080"
+HOST="localhost.pwn.hust.college"
 CONTAINER_NAME = os.environ.get("CONTAINER_NAME", "dojo-test")
 TEST_DOJOS_LOCATION = pathlib.Path(__file__).parent / "dojos"
 
