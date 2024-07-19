b239098b8e9126fdfe0f146b7973d682d0f6d340
CeS-3 <1540962271@qq.com>
2024-07-19 12:37:56 +0800

    update create_dojo CI

--- test/utils.py
+++ test/utils.py
@@ -39,7 +39,7 @@ def make_dojo_official(dojo_rid, admin_session):
 def create_dojo(repository, *, session):
     test_public_key = f"public/{repository}"
     test_private_key = f"private/{repository}"
-    create_dojo_json = { "repository": repository, "public_key": test_public_key, "private_key": test_private_key }
+    create_dojo_json = { "repository": repository, "repository_type":"github" , "public_key": test_public_key, "private_key": test_private_key }
     response = session.post(f"{PROTO}://{HOST}/pwncollege_api/v1/dojo/create", json=create_dojo_json)
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code} - {response.json()}"
     dojo_reference_id = response.json()["dojo"]
