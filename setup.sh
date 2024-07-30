f6986cc043d1b9dd42612dec2b6afef8eab75432
CeS-3 <1540962271@qq.com>
2024-07-30 17:10:16 +0800

    verify if docker is installed or not

--- setup.sh
+++ setup.sh
@@ -7,7 +7,14 @@ MAP_HTTP_PORT=8880
 MAP_HTTPS_PORT=44443
 MAP_SSH_PORT=22222
 
-curl -fsSL https://get.docker.com | /bin/sh
+echo "Checking if docker is installed on the system"
+if command -v docker &> /dev/null
+then
+    echo "Docker is installed"
+else
+    curl -fsSL https://get.docker.com | /bin/sh
+fi
+
 git clone https://github.com/hust-open-atom-club/dojo
 docker build  -t "$IMAGE_NAME" dojo
 docker run --privileged -d -v "dojo:/opt/pwn.college:shared" -p "${MAP_SSH_PORT}:22" -p "${MAP_HTTP_PORT}:80" -p "${MAP_HTTPS_PORT}:443" --name dojo "${IMAGE_NAME}"
