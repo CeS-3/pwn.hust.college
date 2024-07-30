cfae5d04d796bf84e000bd7ce8c6307f40a5ef22
CeS-3 <1540962271@qq.com>
2024-07-30 14:44:31 +0800

    fix github link

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 set -eux
 
-# Default paths, image name and ports
-DOJO_PATH="./dojo"
-CONTAINER_DOJO_PATH="/opt/pwn.college:shared"
-IMAGE_NAME="pwncollege/dojo"
+# Default image name and ports
+IMAGE_NAME="pwnhustcollege/dojo"
 MAP_HTTP_PORT=8880
 MAP_HTTPS_PORT=44443
 MAP_SSH_PORT=22222
 
 curl -fsSL https://get.docker.com | /bin/sh
-git clone https://github.com/HUSTSeclab/dojo.git "$DOJO_PATH"
-docker build  -t "$IMAGE_NAME" "$DOJO_PATH"
-docker run --privileged -d -v "${DOJO_PATH}:${CONTAINER_DOJO_PATH}" -p "${MAP_SSH_PORT}:22" -p "${MAP_HTTP_PORT}:80" -p "${MAP_HTTPS_PORT}:443" --name dojo "${IMAGE_NAME}"
+git clone https://github.com/hust-open-atom-club/dojo
+docker build  -t "$IMAGE_NAME" dojo
+docker run --privileged -d -v "dojo:/opt/pwn.college:shared" -p "${MAP_SSH_PORT}:22" -p "${MAP_HTTP_PORT}:80" -p "${MAP_HTTPS_PORT}:443" --name dojo "${IMAGE_NAME}"
